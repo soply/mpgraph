a033ae29f4711ac6e8f1906602a6daa199874790
Timo Klock <timo@Timos-MacBook-Pro.fritz.box>
2016-12-25 12:56:04 +0100

    Moved create_children methods to subfolder.

---
+++ create_children/create_children_lars.py
@@ -0,0 +1,149 @@
+# coding: utf8
+
+import numpy as np
+from scipy import optimize
+
+from lasso_path_utils import calc_hit_cand, calc_hit_cand_selection
+from mp_utils import calc_B_y_beta, calc_B_y_beta_selection
+
+
+def create_children_lars(support, signum, beta_min, beta_max,
+                         minimiser, svdAAt_U, svdAAt_S, A, y,
+                         additional_indices=None,
+                         used_signs=None,
+                         boundary_parameters=None,
+                         hit_candidates_min=None,
+                         used_signs_min=None,
+                         order_min=None,
+                         hit_candidates_max=None,
+                         used_signs_max=None,
+                         order_max=None,
+                         neglect_entries=None):
+    def get_all_hit_cand(beta):
+        B_beta, y_beta = calc_B_y_beta(A, y, svdAAt_U, svdAAt_S, beta)
+        return calc_hit_cand(B_beta, y_beta, support, signum)
+
+    def candidate_difference(beta, index1, index2):
+        B_betaJ, B_betaI, y_beta = calc_B_y_beta_selection(A, y,
+                                                           svdAAt_U, svdAAt_S,
+                                                           beta, I=support,
+                                                           J=[index1, index2])
+        hit_candidates, used_signs = calc_hit_cand_selection(B_betaI, B_betaJ,
+                                                             y_beta, signum)
+        return (hit_candidates[0] - hit_candidates[1])
+
+    def candidate_difference_squared(beta, index1, index2):
+        B_betaJ, B_betaI, y_beta = calc_B_y_beta_selection(A, y,
+                                                           svdAAt_U, svdAAt_S,
+                                                           beta, I=support,
+                                                           J=[index1, index2])
+        hit_candidates, used_signs = calc_hit_cand_selection(B_betaI, B_betaJ,
+                                                             y_beta, signum)
+        return (hit_candidates[0] - hit_candidates[1]) ** 2
+    # Initialisation
+    if additional_indices is None:
+        hit_candidates_max, used_signs_max = get_all_hit_cand(beta_max)
+        hit_candidates_min, used_signs_min = get_all_hit_cand(beta_min)
+        # Neglect entries that have a minus value either at boun_at_max or
+        # at boun_at_min
+        neglect_entries = np.where(np.logical_or(hit_candidates_min < 0,
+                                         hit_candidates_max < 0))[0]
+        hit_candidates_min[neglect_entries] = -1.0
+        hit_candidates_max[neglect_entries] = -1.0
+        order_max = np.argsort(hit_candidates_max)
+        order_min = np.argsort(hit_candidates_min)
+        additional_indices = [order_max[-1]]
+        used_signs = [used_signs_max[order_max[-1]]]
+        boundary_parameters = []
+        boundary_parameters.append((hit_candidates_max[order_max[-1]],
+                                    beta_max))
+        boundary_parameters.append((hit_candidates_min[order_min[-1]],
+                                    beta_min))
+    if order_min[-1] == order_max[-1]:
+        pass
+    elif order_min[-1] == order_max[-2] and \
+            order_min[-2] == order_max[-1]:
+        # Search for crossing
+        if minimiser == "scipy_bounded":
+            res = optimize.minimize_scalar(candidate_difference_squared,
+                                           args=(order_min[-1], order_max[-1]),
+                                           bounds=(beta_min, beta_max),
+                                           method="Bounded")
+            x = res.x
+            nfev = res.nfev
+            fun = res.fun
+            success = res.success
+        elif minimiser == "scipy_brentq":
+            fminfun = optimize.brentq
+        elif minimiser == "scipy_brenth":
+            fminfun = optimize.brenth
+        elif minimiser == "scipy_ridder":
+            fminfun = optimize.ridder
+        elif minimiser == "scipy_bisect":
+            fminfun = optimize.bisect
+        if minimiser in ["scipy_brentq", "scipy_brenth",
+                         "scipy_ridder", "scipy_bisect"]:
+            x, res = fminfun(candidate_difference, beta_min,
+                             beta_max, xtol=1e-12,
+                             args=(order_max[-1], order_min[-1]),
+                             full_output=True)
+            nfev = res.function_calls
+            fun = 1e-12
+            success = res.converged
+        if success:
+            hit_candidates_cross, used_signs_cross = get_all_hit_cand(x)
+            additional_indices.append(order_min[-1])
+            used_signs.append(used_signs_max[order_min[-1]])
+            boundary_parameters.insert(-1, (hit_candidates_cross[order_min[-1]],
+                                            x))
+        else:
+            print """Warning: Could not find a crossing even if was suppposed to
+                    find crossing at {0}, {1}, {2}, {3}""".format(x, res, fun,
+                                                                  nfev)
+    else:
+        # Bisection step
+        beta_mid = 0.5 * (beta_min + beta_max)
+        hit_candidates_mid, used_signs_mid = get_all_hit_cand(beta_mid)
+        hit_candidates_mid[neglect_entries] = -1.0
+        order_mid = np.argsort(hit_candidates_mid)
+        if order_mid[-1] != order_max[-1]:
+            create_children_lars(support, signum, beta_mid, beta_max,
+                               minimiser, svdAAt_U, svdAAt_S, A, y,
+                               additional_indices=additional_indices,
+                               used_signs=used_signs,
+                               boundary_parameters=boundary_parameters,
+                               hit_candidates_min=hit_candidates_mid,
+                               used_signs_min=used_signs_mid,
+                               order_min=order_mid,
+                               hit_candidates_max=hit_candidates_max,
+                               used_signs_max=used_signs_max,
+                               order_max=order_max,
+                               neglect_entries=neglect_entries)
+        if order_mid[-1] != order_min[-1]:
+            create_children_lars(support, signum, beta_min, beta_mid,
+                               minimiser, svdAAt_U, svdAAt_S, A, y,
+                               additional_indices=additional_indices,
+                               used_signs=used_signs,
+                               boundary_parameters=boundary_parameters,
+                               hit_candidates_min=hit_candidates_min,
+                               used_signs_min=used_signs_min,
+                               order_min=order_min,
+                               hit_candidates_max=hit_candidates_mid,
+                               used_signs_max=used_signs_mid,
+                               order_max=order_mid,
+                               neglect_entries=neglect_entries)
+    return additional_indices, boundary_parameters, used_signs
+
+def lars_post_process_children(additional_indices, boundary_parameters,
+        used_signs, old_support, old_signum):
+    region_refinement = []
+    for i, (index, sign) in enumerate(zip(additional_indices, used_signs)):
+        new_support = np.append(old_support, index)
+        new_signum = np.append(old_signum, sign)
+        order = np.argsort(new_support)
+        new_support, new_signum = new_support[order], new_signum[order]
+        param_min = boundary_parameters[i+1]
+        param_max = boundary_parameters[i]
+        region_refinement.append([param_min, param_max, new_support,
+            new_signum])
+    return region_refinement
---
+++ create_children/lasso_path_utils.py
@@ -0,0 +1,180 @@
+#coding: utf8
+import numpy as np
+
+def calc_hit_cand_selection(AI, AJ, y, signum):
+    """ This method calculates the 'hit candidates' for the Lasso path algorithm.
+    The hit candidates correspond to a subgradient that hits the required
+    boundary |s| < 1, i.e. for alpha > alpha_hit[j] subgradient to index j would
+    exceed the boudary, and thus the index should be taken into the active set.
+
+    Parameters
+    ----------
+    AI : array, shape (n_measurements, n_current_support)
+        The sensing matrix A in the problem 1/2 || Ax - y ||_2^2 + alpha ||x||_1
+        restricted to the index set given by support.
+
+    AJ : array, shape (n_measurements, n_current_support)
+        The sensing matrix A in 1/2 || Ax - y ||_2^2 + alpha ||x||_1
+        restricted to the indices to which we want to calculate the hit value.
+
+    y : array, shape (n_measurements)
+        The vector of measurements
+
+    signum : array, shape (n_current_support)
+        Contains the signs of the coefficients that are currently active, e.g.
+        signum = np.array([1., -1., 1.]).
+    """
+    AtyJ = AJ.T.dot(y)
+    if len(signum) == 0:
+        hit_cand = np.abs(AtyJ)
+        use_sign = np.sign(AtyJ)
+    else:
+        AjT_Ai_inverse_AtA = AJ.T.dot(AI).dot(\
+            np.linalg.inv(AI.T.dot(AI)))
+        hit_top = (AJ.T.dot(np.identity(y.shape[0])) - \
+            AjT_Ai_inverse_AtA.dot(AI.T)).dot(y)
+        # Check if plus or minus gives the upper bound for alpha
+        # FIXME: Note that Tibsharani paper can not transferred to this 1-by-1
+        # FIXME 2: Rethink this...
+        aux_bot = AjT_Ai_inverse_AtA.dot(signum)
+        use_sign = np.sign(hit_top)
+        # In special cases we have to override this choice! See below
+        # use_sign[aux_bot > 1.0] = -1.0 # In this case it is a negative value anyway...
+        # use_sign[aux_bot < (-1.0)] = 1.0
+        hit_bot = use_sign - aux_bot
+        hit_cand = np.divide(hit_top, hit_bot)
+    return hit_cand, use_sign
+
+def calc_hit_cand(A, y, support, signum):
+    """ This method calculates the 'hit candidates' for the Lasso path algorithm.
+    The hit candidates correspond to a subgradient that hits the required
+    boundary |s| < 1, i.e. for alpha > alpha_hit[j] subgradient to index j would
+    exceed the boudary, and thus the index should be taken into the active set.
+
+    Note that in we fill the entries that are related to support in this
+    implementation with -1's in the return vector. Since the support is usually
+    small compared to the number of features, this does not produce a large
+    amount of overhead.
+
+    Parameters
+    ----------
+    A : array, shape (n_measurements, n_features)
+        The sensing matrix A in the problem 1/2 || Ax - y ||_2^2 + alpha ||x||_1.
+
+    y : array, shape (n_measurements)
+        The vector of measurements
+
+    support : array, shape (n_current_support)
+        Contains the indices to the current active support, e.g.
+        support = np.array([5, 10, 11]).
+
+    signum : array, shape (n_current_support)
+        Contains the signs of the coefficients that are currently active, e.g.
+        signum = np.array([1, -1, 1]).
+    """
+    full_index_set = np.arange(A.shape[1])
+    J = np.setdiff1d(full_index_set, support)
+    AJ = A[:, J]
+    AI = A[:, support]
+    hit_cand_J, use_sign_J = calc_hit_cand_selection(AI, AJ, y, signum)
+    hit_cand = -1.0 * np.ones(A.shape[1])
+    use_sign = np.zeros(A.shape[1])
+    use_sign[J] = use_sign_J
+    hit_cand[J] = hit_cand_J
+    return hit_cand, use_sign
+
+def calc_cross_cand_selection(AI, y, signum, indices):
+    """ This method calculates the "cross candidates" for the Lasso path
+    algorithm. Such cross candidates correspond to regularisation parameters
+    at which an entry which was previously active in the support becomes zero
+    again, hence the entry crosses/hits zero. Therefore such entries are kicked
+    out of the active set when such a candidate is the next regularisation
+    parameter in the grid.
+
+    Parameters
+    ----------
+    A : array, shape (n_measurements, n_features)
+        The sensing matrix A in the problem 1/2 || Ax - y ||_2^2 + alpha ||x||_1.
+
+    y : array, shape (n_measurements)
+        The vector of measurements
+
+    signum : array, shape (n_current_support)
+        Contains the signs of the coefficients that are currently active, e.g.
+        signum = np.array([1, -1, 1]).
+
+    indices: array, shape (n_indices)
+        Contains the indices that are of interested ie. to which we want to
+        calculate the cross value.
+    """
+    if len(signum) == 0:
+        cross_candidates = np.array([])
+    else:
+        inverseAtA = np.linalg.inv(AI.T.dot(AI))[indices,:]
+        cross_top = inverseAtA.dot(AI.T).dot(y)
+        cross_bot = inverseAtA.dot(signum)
+        cross_candidates = np.divide(cross_top, cross_bot)
+    return cross_candidates
+
+def calc_cross_cand(A, y, support, signum):
+    """ This method calculates the "cross candidates" for the Lasso path
+    algorithm. Such cross candidates correspond to regularisation parameters
+    at which an entry which was previously active in the support becomes zero
+    again, hence the entry crosses/hits zero. Therefore such entries are kicked
+    out of the active set when such a candidate is the next regularisation
+    parameter in the grid.
+
+    Note that in we fill the entries that are related to support in this
+    implementation with -1's in the return vector. Since the support is usually
+    small compared to the number of features, this does not produce a large
+    amount of overhead.
+
+    Parameters
+    ----------
+    A : array, shape (n_measurements, n_features)
+        The sensing matrix A in the problem 1/2 || Ax - y ||_2^2 + alpha ||x||_1.
+
+    y : array, shape (n_measurements)
+        The vector of measurements
+
+    support : array, shape (n_current_support)
+        Contains the indices to the current active support, e.g.
+        support = np.array([5, 10, 11]).
+
+    signum : array, shape (n_current_support)
+        Contains the signs of the coefficients that are currently active, e.g.
+        signum = np.array([1, -1, 1]).
+    """
+    AI = A[:,support]
+    cross_cand_I = calc_cross_cand_selection(AI, y, signum,
+                                             np.arange(AI.shape[1]))
+    cross_candidates = -1.0 * np.ones(A.shape[1])
+    cross_candidates[support] = cross_cand_I
+    return cross_candidates
+
+def calc_all_cand(A, y, support, signum):
+    """ This method calculates both cross and ht candidates for the Lasso path
+    algorithm and stores them in a single vector. Read function documentations
+    to cross and hit candidate calculation to obtain further information about
+    what a specific candidate means for the solution.
+
+    Parameters
+    ----------
+    A : array, shape (n_measurements, n_features)
+        The sensing matrix A in the problem 1/2 || Ax - y ||_2^2 + alpha ||x||_1.
+
+    y : array, shape (n_measurements)
+        The vector of measurements
+
+    support : array, shape (n_current_support)
+        Contains the indices to the current active support, e.g.
+        support = np.array([5, 10, 11]).
+
+    signum : array, shape (n_current_support)
+        Contains the signs of the coefficients that are currently active, e.g.
+        signum = np.array([1, -1, 1]).
+    """
+    cross_candidates = calc_cross_cand(A, y, support, signum)
+    candidates, used_sign = calc_hit_cand(A, y, support, signum)
+    candidates[support] = cross_candidates[support]
+    return candidates, used_sign
