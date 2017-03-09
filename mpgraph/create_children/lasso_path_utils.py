91c429d98a37d24c4f286739ec3c5a6c2cd5bb56
Timo Klock <timo@Timos-MBP.lan>
2017-03-09 23:52:51 +0100

    Mostly added comments/docs

--- mpgraph/create_children/lasso_path_utils.py
+++ mpgraph/create_children/lasso_path_utils.py
@@ -1,7 +1,14 @@
-#coding: utf8
+# coding: utf8
+""" Implementation of utilities to calculate the Lasso-path knots/regularisation
+    parameters. Different variants are possible, ie. variants to calculate all
+    regularisation parameters, just a subset/selection, or only a specific
+    type. """
+
+__author__ = "Timo Klock"
+
 import numpy as np
 
-def calc_hit_cand_selection(AI, AJ, y, signum, switch_sign = False):
+def calc_hit_cand_selection(AI, AJ, y, signum, prescribed_sign = None):
     """ This method calculates the 'hit candidates' for the Lasso path algorithm.
     The hit candidates correspond to a subgradient that hits the required
     boundary |s| < 1, i.e. for alpha > alpha_hit[j] subgradient to index j would
@@ -24,13 +31,20 @@ def calc_hit_cand_selection(AI, AJ, y, signum, switch_sign = False):
         Contains the signs of the coefficients that are currently active, e.g.
         signum = np.array([1., -1., 1.]).
 
-    switch_sign : boolean
+    prescribed_sign : Integer from {-1, 1} or None if the sign from nominator
+            shall be taken as the sign.
         Allows to use different signs in the calculations of the alpha_hit
         candidates. This becomes necessary in the special case of an entry
         dropping out of the support. From this new tile we essentially have two
         hitting boundaries, both of which belong to different signs. The
         'switch_sign' == False variant gives the upper boundary, ie. the
         boundary where the entry dropped out.
+
+    Returns
+    -----------
+    Potential regularisation parameters for a selection of entries in the
+    support, so-called 'hit candidates' (as a numpy array of size
+    (n_entries_to_J)).
     """
     AtyJ = AJ.T.dot(y)
     if len(signum) == 0:
@@ -43,8 +57,8 @@ def calc_hit_cand_selection(AI, AJ, y, signum, switch_sign = False):
             AjT_Ai_inverse_AtA.dot(AI.T)).dot(y)
         aux_bot = AjT_Ai_inverse_AtA.dot(signum)
         use_sign = np.sign(hit_top)
-        if switch_sign:
-            use_sign = (-1) * use_sign
+        if prescribed_sign is not None:
+            use_sign = prescribed_sign
         hit_bot = use_sign - aux_bot
         hit_cand = np.divide(hit_top, hit_bot)
     return hit_cand, use_sign
@@ -75,6 +89,11 @@ def calc_hit_cand(A, y, support, signum):
     signum : array, shape (n_current_support)
         Contains the signs of the coefficients that are currently active, e.g.
         signum = np.array([1, -1, 1]).
+
+    Returns
+    -----------
+    Potential regularisation parameters for all entries in the support,
+    so-called 'hit candidates' (as a numpy array of size (n_features)).
     """
     full_index_set = np.arange(A.shape[1])
     J = np.setdiff1d(full_index_set, support)
@@ -97,8 +116,8 @@ def calc_cross_cand_selection(AI, y, signum, indices):
 
     Parameters
     ----------
-    A : array, shape (n_measurements, n_features)
-        The sensing matrix A in the problem 1/2 || Ax - y ||_2^2 + alpha ||x||_1.
+    AI : array, shape (n_measurements, n_support)
+        The sensing matrix A restricted to the i'th columns.
 
     y : array, shape (n_measurements)
         The vector of measurements
@@ -110,6 +129,12 @@ def calc_cross_cand_selection(AI, y, signum, indices):
     indices: array, shape (n_indices)
         Contains the indices that are of interested ie. to which we want to
         calculate the cross value.
+
+    Returns
+    -----------
+    Potential crossing candidates for a subset of entries in the support. The
+    crossings candidates are regularisation parameters/knots where a specific
+    entry would hit 0 again.
     """
     if len(signum) == 0:
         cross_candidates = np.array([])
@@ -119,6 +144,40 @@ def calc_cross_cand_selection(AI, y, signum, indices):
         cross_bot = inverseAtA.dot(signum)
         cross_candidates = np.divide(cross_top, cross_bot)
     return cross_candidates
+
+def calc_cross_cand_bottom_selection(AI, signum, indices):
+    """ This method calculates the denominator for "cross candidates" for the
+    LP algorithm. Cross candidates correspond to regularisation parameters
+    at which an entry which was previously active in the support becomes zero
+    again, hence the entry crosses/hits zero. Therefore such entries are kicked
+    out of the active set when such a candidate is the next regularisation
+    parameter in the grid.
+
+    Parameters
+    ----------
+    AI : array, shape (n_measurements, n_support)
+        The sensing matrix A restricted to the i'th columns.
+
+    signum : array, shape (n_current_support)
+        Contains the signs of the coefficients that are currently active, e.g.
+        signum = np.array([1, -1, 1]).
+
+    indices: array, shape (n_indices)
+        Contains the indices that are of interested ie. to which we want to
+        calculate the cross value.
+
+    Returns
+    -----------
+    Denominator of potential crossing candidates for a subset of entries in the
+    support. The crossings candidates are regularisation parameters/knots where
+    a specific entry would hit 0 again.
+    """
+    if len(signum) == 0:
+        cross_bot = np.array([])
+    else:
+        inverseAtA = np.linalg.inv(AI.T.dot(AI))[indices,:]
+        cross_bot = inverseAtA.dot(signum)
+    return cross_bot
 
 def calc_cross_cand(A, y, support, signum):
     """ This method calculates the "cross candidates" for the Lasso path
@@ -148,6 +207,12 @@ def calc_cross_cand(A, y, support, signum):
     signum : array, shape (n_current_support)
         Contains the signs of the coefficients that are currently active, e.g.
         signum = np.array([1, -1, 1]).
+
+    Returns
+    -----------
+    Potential crossing candidates for a all entries in the support. The
+    crossings candidates are regularisation parameters/knots where a specific
+    entry would hit 0 again.
     """
     AI = A[:,support]
     cross_cand_I = calc_cross_cand_selection(AI, y, signum,
@@ -177,6 +242,14 @@ def calc_all_cand(A, y, support, signum):
     signum : array, shape (n_current_support)
         Contains the signs of the coefficients that are currently active, e.g.
         signum = np.array([1, -1, 1]).
+
+    Returns
+    -----------
+    Calculates all candidates for all entries. If an entry is in the support,
+    the respective crossing candidate is calculated (ie. the reg. parameter
+    where the entry hits zero again). If an entry is not in the support, the
+    respective hitting candidate (with sign given by nominator) is calculated.
+    Returns value is numpy array of shape (n_features).
     """
     cross_candidates = calc_cross_cand(A, y, support, signum)
     candidates, used_sign = calc_hit_cand(A, y, support, signum)
